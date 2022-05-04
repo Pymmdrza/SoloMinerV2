07926d2892d5886387f71427dba27f4819bd04b2
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2022-05-04 16:41:31 +0300

    Create context.py

---
+++ context.py
@@ -0,0 +1,25 @@
+fShutdown = False
+listfThreadRunning = [False] * 2
+local_height = 0
+nHeightDiff = {}
+updatedPrevHash = None
+job_id = None
+prevhash = None
+coinb1 = None
+coinb2 = None
+merkle_branch = None
+version = None
+nbits = None
+ntime = None
+clean_jobs = None
+sub_details = None
+extranonce1 = None
+extranonce2_size = None
+
+# =====================================
+# Programmer : Mmdrza
+# Website : https://Mmdrza.Com
+# Email : X4@Mmdrza.Com
+# PyMmdrza.GitHub.io
+# Dev.to/Mmdrza
+# =====================================
