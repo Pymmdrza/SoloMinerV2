f686117d3ffd371eea55d334d5a6c85f94c83a4e
andre04-droid <78146806+andre04-droid@users.noreply.github.com>
2023-09-01 22:30:39 -0500

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = '15tHjg5kkz7FxDbcXzfDGrujC9PdcsYZzD'
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
